07b659f516c63fe48abf6b1db50e9e7f65c48668
Zeynep <zilhan.be@gmail.com>
2020-04-10 12:02:39 +0200

    background card delete edit but

--- controlpanel/her-background.php
+++ controlpanel/her-background.php
@@ -65,6 +65,11 @@
 
                                                                         <p class="ml-3 mb-1"><small><?php echo $bginfo['start_year'] ?>- <?php echo $bginfo['end_year'] ?> / <?php echo $bginfo['levelStudy'] ?></small></p>
                                                                     </div>
+                                                                    <div>
+                                                                            <a><i
+                                                                                    class="fas fa-trash-alt m-1 "></i></a><br>
+                                                                            <a><i class="fas fa-pen m-1 "></i></a>
+                                                                        </div>
                                                                 </div>
                                                             </div>
                                                     <?php } ?>
@@ -116,6 +121,11 @@
 
                                                                     <p class="ml-3 mb-1"><small><?php echo $bginfo2['start_year'] ?>- <?php echo $bginfo2['end_year'] ?> / <?php echo $bginfo2['levelStudy'] ?></small></p>
                                                                 </div>
+                                                                <div>
+                                                                            <a><i
+                                                                                    class="fas fa-trash-alt m-1 "></i></a><br>
+                                                                            <a><i class="fas fa-pen m-1 "></i></a>
+                                                                        </div>
                                                             </div>
                                                         </div>
                                                 <?php } ?>
@@ -172,6 +182,11 @@
 
                                                                     <p class="ml-3 mb-1"><small><?php echo $bginfo4['start_year'] ?>- <?php echo $bginfo4['end_year'] ?> / <?php echo $bginfo4['levelStudy'] ?></small></p>
                                                                 </div>
+                                                                <div>
+                                                                            <a><i
+                                                                                    class="fas fa-trash-alt m-1 "></i></a><br>
+                                                                            <a><i class="fas fa-pen m-1 "></i></a>
+                                                                        </div>
                                                             </div>
                                                         </div>
                                                 <?php } ?>
@@ -223,6 +238,11 @@
 
                                                                         <p class="ml-3 mb-1"><small><?php echo $bginfo3['start_year'] ?>- <?php echo $bginfo3['end_year'] ?> / <?php echo $bginfo3['levelStudy'] ?></small></p>
                                                                     </div>
+                                                                    <div>
+                                                                            <a><i
+                                                                                    class="fas fa-trash-alt m-1 "></i></a><br>
+                                                                            <a><i class="fas fa-pen m-1 "></i></a>
+                                                                        </div>
                                                                 </div>
                                                             </div>
                                                     <?php } ?>
@@ -275,6 +295,11 @@
 
                                                                     <p class="ml-3 mb-1"><small><?php echo $bginfo5['start_year'] ?>- <?php echo $bginfo5['end_year'] ?> / <?php echo $bginfo5['levelStudy'] ?></small></p>
                                                                 </div>
+                                                                <div>
+                                                                            <a><i
+                                                                                    class="fas fa-trash-alt m-1 "></i></a><br>
+                                                                            <a><i class="fas fa-pen m-1 "></i></a>
+                                                                        </div>
                                                             </div>
                                                         </div>
                                                 <?php } ?>
